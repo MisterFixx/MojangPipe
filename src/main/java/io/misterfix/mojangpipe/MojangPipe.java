bf8efc27094245de6ec71bdb77cb7ca6405f0e7a
Mister Fix <me@mc-heads.net>
2019-03-20 18:04:54 +0200

    Added an invalid requests cache to speed up invalid request lookups.

--- src/main/java/io/misterfix/mojangpipe/MojangPipe.java
+++ src/main/java/io/misterfix/mojangpipe/MojangPipe.java
@@ -24,17 +24,19 @@ public class MojangPipe {
     private static final int maxIdleConnections = 100;
     @Parameter(names = {"--cacheLifetime"}, description = "Cache lifetime in minutes before it expires. Default 30")
     private static final int cacheLifetime = 30;
+    @Parameter(names = {"--invalidProfileLifetime"}, description = "The lifetime in minutes of requests before it expires. Default 30")
+    private static final int invalidLifetime = 240;
 
     private static Map<String, String> sessionProfiles = new ConcurrentHashMap<>(), apiUuidProfiles = new ConcurrentHashMap<>(), apiNamesProfiles = new ConcurrentHashMap<>();
-    private static Map<String, Long> sessionRequests = new ConcurrentHashMap<>(), apiUuidRequests = new ConcurrentHashMap<>(), apiNamesRequests = new ConcurrentHashMap<>();
+    private static Map<String, Long> sessionRequests = new ConcurrentHashMap<>(), apiUuidRequests = new ConcurrentHashMap<>(), apiNamesRequests = new ConcurrentHashMap<>(), invalidRequests = new ConcurrentHashMap<>();
     private static Map<String, Object> stats = new ConcurrentHashMap<>();
     private static Map<String, Integer> apiStatusCodes = new ConcurrentHashMap<>();
     private static Map<Integer, Integer> proxies = new ConcurrentHashMap<Integer, Integer>() {{
-        put(3128, 0);
         put(3129, 0);
         put(3130, 0);
         put(3131, 0);
         put(3132, 0);
+        put(3133, 0);
     }};
 
     private static final ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(10);
@@ -48,7 +50,14 @@ public class MojangPipe {
             String uuid = request.params(":uuid");
             String json = "";
 
-            if(sessionRequests.containsKey(uuid) && (System.currentTimeMillis() - sessionRequests.get(uuid)) < (cacheLifetime * 60000)){
+            if(invalidRequests.containsKey(uuid) && (System.currentTimeMillis() - invalidRequests.get(uuid)) < (invalidLifetime * 60000)){
+                response.status(204);
+                threadPool.execute(() -> {
+                    stats.put("profile_from_mem", ((int) stats.getOrDefault("profile_from_api", 0)) + 1);
+                    System.out.println("Served profile for UUID " + uuid + " (from invalid requests cache)");
+                });
+                return "";
+            } else if(sessionRequests.containsKey(uuid) && (System.currentTimeMillis() - sessionRequests.get(uuid)) < (cacheLifetime * 60000)){
                 json = sessionProfiles.get(uuid);
                 String finalJson = json;
                 threadPool.execute(()->{
@@ -56,8 +65,7 @@ public class MojangPipe {
                     stats.put("bytes_served", ((long)stats.getOrDefault("bytes_served", 0L))+ finalJson.length());
                     System.out.println("Served profile for UUID "+uuid+" (from memory)");
                 });
-            }
-            else{
+            } else {
                 Request apiRequest = new Request.Builder().url("https://sessionserver.mojang.com/session/minecraft/profile/"+uuid).build();
                 OkHttpClient client = Utils.getClient(proxies, connectionPool);
                 Response apiResponse = client.newCall(apiRequest).execute();
@@ -80,6 +88,8 @@ public class MojangPipe {
 
                     String finalJson1 = json;
                     threadPool.execute(() -> stats.put("bytes_served", ((long) stats.getOrDefault("bytes_served", 0L)) + finalJson1.length()));
+                } else if(responseCode == 204){
+                    invalidRequests.put(uuid, System.currentTimeMillis());
                 }
             }
 
@@ -91,7 +101,14 @@ public class MojangPipe {
             String name = request.params(":name");
             String json = "";
 
-            if(apiUuidRequests.containsKey(name) && (System.currentTimeMillis() - apiUuidRequests.get(name)) < (cacheLifetime * 60000)){
+            if(invalidRequests.containsKey(name) && (System.currentTimeMillis() - invalidRequests.get(name)) < (invalidLifetime * 60000)){
+                response.status(204);
+                threadPool.execute(() -> {
+                    stats.put("uuid_from_mem", ((int) stats.getOrDefault("uuid_from_mem", 0)) + 1);
+                    System.out.println("Served UUID lookup for username " + name + " (from invalid requests cache)");
+                });
+                return "";
+            } else if(apiUuidRequests.containsKey(name) && (System.currentTimeMillis() - apiUuidRequests.get(name)) < (cacheLifetime * 60000)){
                 json = apiUuidProfiles.get(name);
                 String finalJson = json;
                 threadPool.execute(()->{
@@ -123,6 +140,8 @@ public class MojangPipe {
 
                     String finalJson1 = json;
                     threadPool.execute(() -> stats.put("bytes_served", ((long) stats.getOrDefault("bytes_served", 0L)) + finalJson1.length()));
+                } else if(responseCode == 204){
+                    invalidRequests.put(name, System.currentTimeMillis());
                 }
             }
 
@@ -134,7 +153,14 @@ public class MojangPipe {
             String uuid = request.params(":uuid");
             String json = "";
 
-            if(apiNamesRequests.containsKey(uuid) && (System.currentTimeMillis() - apiNamesRequests.get(uuid)) < (cacheLifetime * 60000)){
+            if(invalidRequests.containsKey(uuid) && (System.currentTimeMillis() - invalidRequests.get(uuid)) < (invalidLifetime * 60000)){
+                response.status(204);
+                threadPool.execute(() -> {
+                    stats.put("names_from_mem", ((int) stats.getOrDefault("names_from_api", 0)) + 1);
+                    System.out.println("Served names list for UUID " + uuid + " (from invalid requests cache)");
+                });
+                return "";
+            } else if(apiNamesRequests.containsKey(uuid) && (System.currentTimeMillis() - apiNamesRequests.get(uuid)) < (cacheLifetime * 60000)){
                 json = apiNamesProfiles.get(uuid);
                 String finalJson = json;
                 threadPool.execute(()->{
@@ -167,6 +193,8 @@ public class MojangPipe {
 
                     String finalJson1 = json;
                     threadPool.execute(() -> stats.put("bytes_served", ((long) stats.getOrDefault("bytes_served", 0L)) + finalJson1.length()));
+                } else if(responseCode == 204){
+                    invalidRequests.put(uuid, System.currentTimeMillis());
                 }
             }
 
@@ -231,6 +259,11 @@ public class MojangPipe {
                 if((System.currentTimeMillis() - time) > (cacheLifetime * 60000)){
                     sessionRequests.remove(uuid);
                     sessionProfiles.remove(uuid);
+                }
+            });
+            invalidRequests.forEach((name, time) -> {
+                if((System.currentTimeMillis() - time) > (invalidLifetime * 60000)){
+                    invalidRequests.remove(name);
                 }
             });
         }, 10, 10, TimeUnit.MINUTES);
